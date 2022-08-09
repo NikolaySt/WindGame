c2679b25196e0dc5b5e4744a35a405e6c6b39863
Nikolay Stoychev <n_stoychev@yahoo.com>
2022-08-09 11:22:41 +0200

    Add project files.

---
+++ WindGame/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using ConsoleApp8;
+
+Console.WriteLine("Hello, World!");
+
+const int rows = 10000;
+const int cols = 10000;
+var field = new int[rows, cols];
+var random = new Random();
+var unrolledRandomValues = Enumerable.Repeat(0, rows * cols).Select(i => random.Next(1, 5)).ToArray();
+
+var elementSize = Marshal.SizeOf(typeof(int));
+
+Buffer.BlockCopy(unrolledRandomValues, 0, field, 0, rows * cols * elementSize);
+
+var wind = "UULDDDULULULULLL";
+
+var watch = new Stopwatch();
+watch.Start();
+var sum = Solution.SumLeftLeavesAfterStorm(field, wind);
+watch.Stop();
+
+Console.WriteLine($"Matrix = [{rows}x{cols}]={rows * cols}, Winds {wind}, Sum {sum}, CalculationTime: {watch.Elapsed}");
+
+Console.ReadLine();
---
+++ WindGame/Solution.cs
@@ -0,0 +1,99 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ConsoleApp8
+{
+    public static class Solution
+    {
+        public static int SumLeftLeavesAfterStorm(int[,] field, string winds)
+        {
+            var result = field.Clone() as int[,];
+            foreach (var wind in winds)
+            {
+                result = wind switch
+                {
+                    'U' => ShiftUp(result),
+                    'D' => ShiftDown(result),
+                    'L' => ShiftLeft(result),
+                    'R' => ShiftRight(result),
+                    _ => result
+                };
+
+                //Console.WriteLine(wind);
+                //Print(result);
+            }
+
+            var elementSize = Marshal.SizeOf(typeof(int));
+            var rows = field.GetLength(0);
+            var cols = field.GetLength(1);
+            var unRolledResult = new int[rows * cols];
+            Buffer.BlockCopy(result, 0, unRolledResult, 0, rows * cols * elementSize);
+            return unRolledResult.Sum();
+        }
+
+        private static void Print(int[,] field)
+        {
+            Console.WriteLine("------------------------");
+            for (var i = 0; i < field.GetLength(0); i++)
+            {
+                for (var j = 0; j < field.GetLength(1); j++)
+                    Console.Write(field[i, j] + " ");
+                Console.WriteLine();
+            }
+            Console.WriteLine("------------------------");
+        }
+
+        private static int GetElementSize<T>()
+        {
+            return Marshal.SizeOf(typeof(T));
+        }
+
+        private static (int, int) GetSize(int[,] field)
+        {
+            var rows = field.GetLength(0);
+            var cols = field.GetLength(1);
+            return (rows, cols);
+        }
+
+        private static int[,] ShiftDown(int[,] field)
+        {
+            var (rows, cols) = GetSize(field);
+            var result = new int[rows, cols];
+            Array.Copy(field, 0, result, rows, rows * cols - rows);
+            return result;
+        }
+
+        private static int[,] ShiftUp(int[,] field)
+        {
+            var (rows, cols) = GetSize(field);
+            var result = new int[rows, cols];
+            Array.Copy(field, rows, result, 0, rows * cols - rows);
+            return result;
+        }
+
+        private static int[,] ShiftLeft(int[,] field)
+        {
+            var (rows, cols) = GetSize(field);
+            var elementSize = GetElementSize<int>();
+            for (var row = 0; row < rows; row++)
+            {
+                var shiftRow = new int[cols];
+                Buffer.BlockCopy(field, row * (cols) * elementSize + elementSize, shiftRow, 0, (cols - 1) * elementSize);
+                Buffer.BlockCopy(shiftRow, 0, field, row * cols * elementSize, cols * elementSize);
+            }
+            return field;
+        }
+
+        private static int[,] ShiftRight(int[,] field)
+        {
+            var (rows, cols) = GetSize(field);
+            var elementSize = GetElementSize<int>();
+            for (var row = 0; row < rows; row++)
+            {
+                var shiftRow = new int[cols];
+                Buffer.BlockCopy(field, row * (cols) * elementSize, shiftRow, elementSize, (cols - 1) * elementSize);
+                Buffer.BlockCopy(shiftRow, 0, field, row * cols * elementSize, cols * elementSize);
+            }
+            return field;
+        }
+    }
+}
